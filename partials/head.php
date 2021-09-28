fe891f13fffa47a3c24f37df702c0ca3ae41b77f
Martin Mbithi <martdevelopers254@gmail.com>
2021-09-28 12:55:04 +0300

    Implement Side Navigation Bar

--- partials/head.php
+++ partials/head.php
@@ -1,18 +1,26 @@
-<!DOCTYPE html>
-<html>
+<?php
+$ret = "SELECT * FROM settings";
+$stmt = $mysqli->prepare($ret);
+$stmt->execute(); //ok
+$res = $stmt->get_result();
+while ($sys = $res->fetch_object()) {
+?>
+    <!DOCTYPE html>
+    <html>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <title>Museum Information Management System</title>
-    <!-- Favicon-->
-    <link rel="icon" href="favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
-    <!-- Custom Css -->
-    <link rel="stylesheet" href="assets/css/main.css">
-    <link rel="stylesheet" href="assets/css/authentication.css">
-    <link rel="stylesheet" href="assets/css/all-themes.css" />
-    <!-- Load Alerts -->
-    <link rel="stylesheet" href="assets/plugins/iziToast/iziToast.min.css">
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+        <title><?php echo $sys->name; ?> | Information Management System</title>
+        <!-- Favicon-->
+        <link rel="icon" href="favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+        <!-- Custom Css -->
+        <link rel="stylesheet" href="assets/css/main.css">
+        <link rel="stylesheet" href="assets/css/authentication.css">
+        <link rel="stylesheet" href="assets/css/all-themes.css" />
+        <!-- Load Alerts -->
+        <link rel="stylesheet" href="assets/plugins/iziToast/iziToast.min.css">
+    </head>
+<?php } ?>
