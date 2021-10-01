2d308218b948c6227b073dda0318ad21942c4c43
Martin Mbithi <martdevelopers254@gmail.com>
2021-10-01 15:01:32 +0300

    Implement Member List Page

--- modules_memberships_members.php
+++ modules_memberships_members.php
@@ -4,15 +4,16 @@ require_once('config/config.php');
 require_once('config/checklogin.php');
 require_once('config/codeGen.php');
 checklogin();
-/* Add Staff */
-if (isset($_POST['Add_Staff'])) {
+
+/* Add Member */
+if (isset($_POST['Add_Member'])) {
     $user_id = $sys_gen_id;
     $user_name = $_POST['user_name'];
     $user_phone = $_POST['user_phone'];
     $user_password = sha1(md5($_POST['user_password']));
     $user_email = $_POST['user_email'];
     $user_idno = $_POST['user_idno'];
-    $user_access_level = 'Staff';
+    $user_access_level = 'Member';
     $user_created_on = date('d, M Y');
 
     /* Prevent Double Entries */
@@ -35,20 +36,17 @@ if (isset($_POST['Add_Staff'])) {
         }
     }
 }
+
 /* Delete Staff */
 if (isset($_GET['delete'])) {
     $delete = $_GET['delete'];
-    /* Log Staff Out If They Delete Their Account */
-    $user_id = $_SESSION['user_id'];
     $adn = "DELETE FROM users WHERE user_id=?";
     $stmt = $mysqli->prepare($adn);
     $stmt->bind_param('s', $delete);
     $stmt->execute();
     $stmt->close();
-    if ($stmt && ($user_id == $delete)) {
-        $success = "Deleted" && header("refresh:1; url=logout");
-    } else if ($stmt && $user_id != $delete) {
-        $success = "Deleted" && header("refresh:1; url=modules_hrm");
+    if ($stmt) {
+        $success = "Deleted" && header("refresh:1; url=modules_memberships_members");
     } else {
         $err = "Please Try Again Later";
     }
@@ -77,13 +75,14 @@ require_once('partials/head.php');
             <div class="block-header">
                 <div class="row">
                     <div class="col-lg-12 col-md-6 col-sm-7">
-                        <h2>Human Resource Management Module</h2>
+                        <h2>Members</h2>
                         <ul class="breadcrumb">
                             <li class="breadcrumb-item"><a href="dashboard">Dashboard</a></li>
-                            <li class="breadcrumb-item active">HRM</li>
+                            <li class="breadcrumb-item"><a href="">Memberships</a></li>
+                            <li class="breadcrumb-item active">Members</li>
                         </ul>
                         <div class="text-right">
-                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#add_modal">Add Staff</button>
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#add_modal">Register New Member</button>
                         </div>
                     </div>
                 </div>
@@ -99,27 +98,25 @@ require_once('partials/head.php');
                                     <th>Phone Number</th>
                                     <th>Email Adr</th>
                                     <th>ID Number</th>
-                                    <th>Access Level</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <?php
-                                $ret = "SELECT * FROM users WHERE user_access_level != 'Member'  ";
+                                $ret = "SELECT * FROM users WHERE user_access_level = 'Member'";
                                 $stmt = $mysqli->prepare($ret);
                                 $stmt->execute(); //ok
                                 $res = $stmt->get_result();
-                                while ($users = $res->fetch_object()) {
+                                while ($members = $res->fetch_object()) {
                                 ?>
                                     <tr>
                                         <td>
-                                            <a href="modules_hrm_profile?view=<?php echo $users->user_id; ?>">
-                                                <?php echo $users->user_name; ?>
+                                            <a href="modules_memberships_member?view=<?php echo $members->user_id; ?>">
+                                                <?php echo $members->user_name; ?>
                                             </a>
                                         </td>
-                                        <td><?php echo $users->user_phone; ?></td>
-                                        <td><?php echo $users->user_email; ?></td>
-                                        <td><?php echo $users->user_idno; ?></td>
-                                        <td><?php echo $users->user_access_level; ?></td>
+                                        <td><?php echo $members->user_phone; ?></td>
+                                        <td><?php echo $members->user_email; ?></td>
+                                        <td><?php echo $members->user_idno; ?></td>
                                     </tr>
                                 <?php
                                 } ?>
@@ -135,55 +132,81 @@ require_once('partials/head.php');
         <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title" id="largeModalLabel">Register New Staff</h4>
+                    <h4 class="modal-title" id="largeModalLabel">Register New Member</h4>
                 </div>
                 <form method="POST">
                     <div class="modal-body">
-                        <div class="row clearfix">
-                            <div class="col-sm-12">
-                                <div class="form-group">
-                                    <label>Full Name</label>
-                                    <div class="form-line">
-                                        <input type="text" name="user_name" required class="form-control" />
+                        <fieldset class="border border-primary p-2">
+                            <legend class="w-auto text-primary"><small> Member Personal Information </small></legend>
+                            <div class="row clearfix">
+                                <div class="col-sm-12">
+                                    <div class="form-group">
+                                        <label>Full Name</label>
+                                        <div class="form-line">
+                                            <input type="text" name="user_name" required class="form-control" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label>Phone Number</label>
+                                        <div class="form-line">
+                                            <input type="text" name="user_phone" required class="form-control" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label>National ID No</label>
+                                        <div class="form-line">
+                                            <input type="text" name="user_idno" required class="form-control" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label>Email</label>
+                                        <div class="form-line">
+                                            <input type="text" name="user_email" required class="form-control" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label>Password</label>
+                                        <div class="form-line">
+                                            <input type="password" name="user_password" required class="form-control" />
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-sm-6">
-                                <div class="form-group">
-                                    <label>Phone Number</label>
-                                    <div class="form-line">
-                                        <input type="text" name="user_phone" required class="form-control" />
+                        </fieldset>
+                        <fieldset class="border border-primary p-2">
+                            <legend class="w-auto text-primary"><small> Membership Package </small></legend>
+                            <div class="row clearfix">
+                                <div class="col-sm-12">
+                                    <div class="form-group">
+                                        <label>Select Package Name</label>
+                                        <div class="form-line">
+                                            <select name="user_membership_package_package_id" class="form control">
+                                                <?php
+                                                $ret = "SELECT * FROM membership_packages  ";
+                                                $stmt = $mysqli->prepare($ret);
+                                                $stmt->execute(); //ok
+                                                $res = $stmt->get_result();
+                                                while ($package = $res->fetch_object()) {
+                                                ?>
+                                                    <option value="<?php echo $package->package_id; ?>"><?php echo $package->package_name; ?></option>
+                                                <?php } ?>
+                                            </select>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-sm-6">
-                                <div class="form-group">
-                                    <label>National ID No</label>
-                                    <div class="form-line">
-                                        <input type="text" name="user_idno" required class="form-control" />
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-sm-6">
-                                <div class="form-group">
-                                    <label>Email</label>
-                                    <div class="form-line">
-                                        <input type="text" name="user_email" required class="form-control" />
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-sm-6">
-                                <div class="form-group">
-                                    <label>Password</label>
-                                    <div class="form-line">
-                                        <input type="password" name="user_password" required class="form-control" />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        </fieldset>
                     </div>
                     <div class="modal-footer">
-                        <button type="submit" name="Add_Staff" class="btn btn-link waves-effect">SAVE CHANGES</button>
+                        <button type="submit" name="Add_Member" class="btn btn-link waves-effect">SAVE CHANGES</button>
                         <button type="button" class="btn btn-link waves-effect" data-dismiss="modal">CLOSE</button>
                     </div>
                 </form>
