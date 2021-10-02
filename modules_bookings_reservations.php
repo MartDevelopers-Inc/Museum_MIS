932c062b06b46497a75554b1bd103a1b1aa2d660
Martin Mbithi <martdevelopers254@gmail.com>
2021-10-02 10:39:04 +0300

    Craft Rooms

---
+++ modules_bookings_reservations.php
@@ -0,0 +1,173 @@
+<?php
+session_start();
+require_once('config/config.php');
+require_once('config/checklogin.php');
+require_once('config/codeGen.php');
+checklogin();
+
+/* Membership Package */
+if (isset($_POST['Add_Package'])) {
+    $package_id = $sys_gen_id;
+    $package_name = $_POST['package_name'];
+    $package_details = $_POST['package_details'];
+    $package_pricing = $_POST['package_pricing'];
+
+    /* Prevent Double Entries */
+    $sql = "SELECT * FROM  membership_packages WHERE package_name = '$package_name'";
+    $res = mysqli_query($mysqli, $sql);
+    if (mysqli_num_rows($res) > 0) {
+        $row = mysqli_fetch_assoc($res);
+        if ($package_name == $row['package_name']) {
+            $err =  "Package Name Already Exists";
+        }
+    } else {
+        $query = "INSERT INTO membership_packages (package_id, package_pricing,  package_name, package_details) VALUES(?,?,?,?)";
+        $stmt = $mysqli->prepare($query);
+        $rc = $stmt->bind_param('ssss', $package_id, $package_pricing, $package_name, $package_details);
+        $stmt->execute();
+        if ($stmt) {
+            $success = "$package_name Added";
+        } else {
+            $info = "Please Try Again Or Try Later";
+        }
+    }
+}
+
+/* Delete Membership Package */
+if (isset($_GET['delete'])) {
+    $delete = $_GET['delete'];
+    $adn = "DELETE FROM membership_packages WHERE package_id=?";
+    $stmt = $mysqli->prepare($adn);
+    $stmt->bind_param('s', $delete);
+    $stmt->execute();
+    $stmt->close();
+    if ($stmt) {
+        $success = "Deleted" && header("refresh:1; url=modules_memberships_packages");
+    } else {
+        $err = "Please Try Again Later";
+    }
+}
+
+require_once('partials/head.php');
+?>
+
+<body class="theme-red">
+    <!-- Page Loader -->
+    <div class="page-loader-wrapper">
+        <?php require_once('partials/loader.php'); ?>
+    </div>
+
+    <!-- Overlay For Sidebars -->
+    <div class="overlay"></div>
+
+    <!-- Top Bar -->
+    <?php require_once('partials/navigation.php'); ?>
+
+    <!-- Left Sidebar -->
+    <?php require_once('partials/sidebar.php'); ?>
+
+    <!-- Add Staff Modal -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="block-header">
+                <div class="row">
+                    <div class="col-lg-12 col-md-6 col-sm-7">
+                        <h2>Museum Membership Packages</h2>
+                        <ul class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="dashboard">Dashboard</a></li>
+                            <li class="breadcrumb-item active">Memberships</li>
+                        </ul>
+                        <div class="text-right">
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#add_modal">Add Membership Package</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="row clearfix">
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-striped table-hover js-basic-example dataTable">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Fee</th>
+                                    <th>Details</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $ret = "SELECT * FROM membership_packages  ";
+                                $stmt = $mysqli->prepare($ret);
+                                $stmt->execute(); //ok
+                                $res = $stmt->get_result();
+                                while ($packages = $res->fetch_object()) {
+                                ?>
+                                    <tr>
+                                        <td>
+                                            <a href="modules_memberships_package?view=<?php echo $packages->package_id; ?>">
+                                                <?php echo $packages->package_name; ?>
+                                            </a>
+                                        </td>
+                                        <td>Ksh <?php echo $packages->package_pricing; ?></td>
+                                        <td><?php echo $packages->package_details; ?></td>
+                                    </tr>
+                                <?php
+                                } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Add Modal -->
+    <div class="modal fade" id="add_modal" tabindex="-1" role="dialog">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="largeModalLabel">Register New Memberships Package</h4>
+                </div>
+                <form method="POST">
+                    <div class="modal-body">
+                        <div class="row clearfix">
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label>Package Name</label>
+                                    <div class="form-line">
+                                        <input type="text" name="package_name" required class="form-control" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label>Package Subscription Fee (Ksh)</label>
+                                    <div class="form-line">
+                                        <input type="text" name="package_pricing" required class="form-control" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-12">
+                                <div class="form-group">
+                                    <label>Package Details</label>
+                                    <div class="form-line">
+                                        <textarea type="text" rows="5" name="package_details" class="form-control no-resize auto-growth" required /></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" name="Add_Package" class="btn btn-link waves-effect">SAVE </button>
+                        <button type="button" class="btn btn-link waves-effect" data-dismiss="modal">CLOSE</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Scripts -->
+    <?php require_once('partials/scripts.php'); ?>
+</body>
+
+</html>
