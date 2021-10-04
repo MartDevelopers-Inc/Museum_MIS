ea7818f52e82a9eb33219d17a7fb3d2c694677e5
Martin Mbithi <martdevelopers254@gmail.com>
2021-10-04 12:50:43 +0300

    Payment Records On Reservations

--- modules_bookings_reservation.php
+++ modules_bookings_reservation.php
@@ -2,6 +2,7 @@
 session_start();
 require_once('config/config.php');
 require_once('config/checklogin.php');
+require_once('config/codeGen.php');
 checklogin();
 
 /* Update Reservation Details */
@@ -111,13 +112,15 @@ require_once('partials/head.php');
                                 <!-- Nav tabs -->
                                 <ul class="nav nav-tabs">
                                     <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#reservation_update">Update Reservation</a></li>
-                                    <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#reservation_payment">Reservation Payment</a></li>
+                                    <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#reservation_payment">Reservation Payment Record</a></li>
                                     <?php
                                     $user_access_level = $_SESSION['user_access_level'];
                                     /* Only Show This If Access Level Is Admin */
                                     if ($user_access_level == 'Administrator') {
+                                        if ($reservation->reservation_status != 'Approved') {
                                     ?>
-                                        <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#approve_reservation">Approve Reservation</a></li>
+                                            <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#approve_reservation">Approve Reservation</a></li>
+                                        <?php } ?>
                                         <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#delete_reservation">Delete Reservation</a></li>
                                     <?php } ?>
                                 </ul>
@@ -158,38 +161,86 @@ require_once('partials/head.php');
                                     </div>
 
                                     <div role="tabpanel" class="tab-pane" id="reservation_payment">
-                                        <table class="table table-bordered table-striped table-hover js-basic-example dataTable">
-                                            <thead>
-                                                <tr>
-                                                    <th style="width:40%;">Member Details</th>
-                                                    <th class="number">Accomodations Dates</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody class="no-border-x">
-                                                <?php
-                                                $ret = "SELECT * FROM  accomodations a
-                                                INNER JOIN users u ON a.accomodation_id  = u.user_id 
-                                                INNER JOIN rooms r ON r.room_id = a.accomodation_room_id
-                                                WHERE r.room_id = '$view'";
-                                                $stmt = $mysqli->prepare($ret);
-                                                $stmt->execute(); //ok
-                                                $res = $stmt->get_result();
-                                                while ($reservations = $res->fetch_object()) {
-                                                ?>
-                                                    <tr>
-                                                        <td>
-                                                            Name : <?php echo $reservations->user_name; ?><br>
-                                                            Phone : <?php echo $reservations->user_phone; ?><br>
-                                                            Email : <?php echo $reservations->user_email; ?>
-                                                        </td>
-                                                        <td>
-                                                            Check In:<?php echo $reservations->accomodation_check_indate; ?><br>
-                                                            Check Out: <?php echo $reservations->accomodation_check_out_date; ?>
-                                                        </td>
-                                                    </tr>
-                                                <?php } ?>
-                                            </tbody>
-                                        </table>
+                                        <!-- Payment Record -->
+                                        <div class="col-lg-12 col-md-12 col-sm-12">
+                                            <div class="card">
+                                                <div class="header text-center">
+                                                    <h2>Museum Visit Reservation Payment Receipt</h2>
+                                                    <h4>Receipt # <strong><?php echo $b; ?></strong>
+                                                    </h4>
+                                                </div>
+                                                <div class="body">
+                                                    <hr>
+                                                    <div class="row">
+                                                        <div class="col-md-6 col-sm-6">
+                                                            <address>
+                                                                <strong><?php echo $reservation->user_name; ?></strong><br>
+                                                                Email: <?php echo $reservation->user_email; ?><br>
+                                                                <abbr title="Phone">P:</abbr> <?php echo $reservation->user_phone; ?>
+                                                            </address>
+                                                        </div>
+                                                        <div class="col-md-6 col-sm-6 text-right">
+                                                            <p><strong>Reservation Visit Date: </strong> <?php echo date('M, d Y', strtotime($reservation->reservation_date)); ?></p>
+                                                            <p class="m-t-10"><strong>Reservation Status: </strong>
+                                                                <?php if ($reservation->reservation_status == 'Pending') { ?>
+                                                                    <span class="badge bg-orange">Pending</span>
+                                                                <?php } else {
+                                                                ?>
+                                                                    <span class="badge bg-success">Approved</span>
+                                                                <?php  } ?>
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="mt-40"></div>
+                                                    <div class="row">
+                                                        <div class="col-md-12">
+                                                            <div class="table-responsive">
+                                                                <table id="mainTable" class="table table-striped" style="cursor: pointer;">
+                                                                    <thead>
+                                                                        <tr>
+                                                                            <th>Payment Amount</th>
+                                                                            <th>Confirmation Code</th>
+                                                                            <th>Date Paid</th>
+                                                                        </tr>
+                                                                    </thead>
+                                                                    <tbody>
+                                                                        <?php
+                                                                        /* Load Payment Record Related To This Reservation */
+                                                                        $ret = "SELECT * FROM payments  WHERE payment_service_paid_id = '$view'";
+                                                                        $stmt = $mysqli->prepare($ret);
+                                                                        $stmt->execute(); //ok
+                                                                        $res = $stmt->get_result();
+                                                                        while ($payment_details = $res->fetch_object()) {
+                                                                        ?>
+                                                                            <tr>
+                                                                                <td>Ksh <?php echo $payment_details->payment_amount; ?></td>
+                                                                                <td><?php echo $payment_details->payment_confirmation_code; ?></td>
+                                                                                <td><?php echo date('M, d Y', strtotime($payment_details->payment_created_at)); ?></td>
+                                                                            </tr>
+                                                                        <?php } ?>
+
+                                                                    </tbody>
+                                                                </table>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <hr>
+                                                    <div class="row" style="border-radius: 0px;">
+                                                        <div class="col-md-12 text-right">
+                                                            <p class="text-right"><b>Sub-total:</b> Ksh: <?php echo $payment_details->payment_amount; ?></p>
+                                                            <p class="text-right">Discout: 12.9%</p>
+                                                            <p class="text-right">VAT: 12.9%</p>
+                                                            <hr>
+                                                            <h3 class="text-right">Ksh: <?php echo $payment_details->payment_amount; ?></h3>
+                                                        </div>
+                                                    </div>
+                                                    <hr>
+                                                    <div class="hidden-print col-md-12 text-right">
+                                                        <a href="javascript:window.print()" class="btn btn-raised btn-success"><i class="zmdi zmdi-print"></i></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
 
                                     <div role="tabpanel" class="tab-pane" id="approve_reservation">
